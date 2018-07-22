03a4a8e89af8558b8ce5de1e8582d77fc990f50c
rwarrick85 <ryan.warrick85@gmail.com>
2018-07-21 22:51:42 -0400

    Code change

--- intro2/analyzer.rb
+++ intro2/analyzer.rb
@@ -1,17 +1,51 @@
-puts "Hello, enter your first name please"
+def multiply(first_number, second_number)
+  first_number.to_f * second_number.to_f
+end
 
-first_name = gets.chomp
-puts "Thank you! What is your last name?"
-last_name = gets.chomp
+def divide(first_number, second_number)
+  first_number.to_f / second_number.to_f
+end
 
-puts "Welcome #{first_name} #{last_name}, to the Analyzer Program!"
+def subtract(first_number, second_number)
+  second_number.to_f - first_number.to_f
+end
 
-puts "Your #{first_name} is #{first_name.length} characters long."
-puts "Your #{last_name} is #{last_name.length} characters long."
+def mod(first_number, second_number)
+  first_number.to_f % second_number.to_f
+end
 
-#puts "Here is your first name reversed, #{first_name.reverse}"
-#puts "Here is your last name reversed, #{last_name.reverse}"
+puts "What do you want to do? 1) multiply 2) divide 3)subtract 4) find remainder"
+prompt = gets.chomp
 
-full_name = first_name + " " + last_name
+puts "Enter your first number"
+first_number = gets.chomp
+puts "Enter your second number"
+second_number = gets.chomp
 
-puts "You first and last name in reverse reads #{full_name}"
+if prompt == '1'
+  puts "You have chosen to multiply #{first_number} with #{second_number}"
+  result = multiply(first_number, second_number)
+
+elsif prompt == '2'
+  puts "You have chosen to divide"
+  result = divide(first_number, second_number)
+
+elsif prompt == '3'
+  puts "You have chosen to subtract"
+  result = subtract(second_number, first_number)
+
+elsif prompt == '4'
+  puts "You have chosen to find the remainder"
+  result = mod(first_number, second_number)
+
+else
+  puts "You have made an invalid choice"
+
+end
+
+puts "The result is #{result}"
+# puts "Please enter your first number"
+# first_number = gets.chomp
+# puts "Please enter your second"
+# second_number = gets.chomp
+
--- intro2/playground.rb
+++
@@ -1,8 +0,0 @@
-puts "Hello enter in youur first name"
-
-first_name = gets.chomp
-puts "What is your last name?"
-last_name = gets.chomp
-
-puts "Welcome #{first_name} #{last_name}, to the jungle!"
-
